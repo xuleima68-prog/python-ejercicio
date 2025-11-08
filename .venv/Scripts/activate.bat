39c514d77cd745b74fb4afef4b5d194e771890c4
Xulei <xuleima55@gmail.com>
2025-11-08 22:44:20 +0100

    venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\xulei\Desktop\python\python-ejercicio\.venv
+set VIRTUAL_ENV=c:\Users\xulei\Desktop\Python_Ej\python-ejercicio\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
